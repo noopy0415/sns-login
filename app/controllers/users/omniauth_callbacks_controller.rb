af0b22a83961fbcf26d1fd2bb22836b20a18d4d8
obaranoriya <noopy0415@gmail.com>
2019-03-20 10:46:34 +0900

    リファクタリング

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,120 +1,43 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-    def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    auth = request.env["omniauth.auth"]
-    @user = User.where(provider: auth.provider, provider_uid: auth.uid).first
-    unless @user
-      # ユーザが無い場合は作成
-      @user = User.create(
-          family_name:     auth.extra.raw_info.name,
-          email:    dummy_email(auth),
-          provider: auth.provider,
-          provider_token:    auth.credentials.token,
-          provider_uid: auth.uid,
-          password: Devise.friendly_token[0,20],
-          encrypted_password:[*1..9, *'A'..'Z', *'a'..'z'].sample(10).join,
-          # agreement: true
-      )
-    end
-
-    if @user.persisted?
-      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+  def google_oauth2
+    callback_for(:google)
   end
 
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    auth = request.env["omniauth.auth"]
-    @user = User.where(provider: auth.provider, provider_uid: auth.uid).first
-    unless @user
-      # ユーザが無い場合は作成
-      puts "auth.extra.raw_info.name: #{auth.extra.raw_info.name}"
-      puts "auth.provider: #{auth.provider}"
-      puts "auth.credentials.token: #{auth.credentials.token}"
-      puts "auth.uid: #{auth.uid}"
-      @user = User.create(
-          family_name:     auth.extra.raw_info.name,
-          email:    dummy_email(auth),
-          provider: auth.provider,
-          provider_token:    auth.credentials.token,
-          provider_uid: auth.uid,
-          password: Devise.friendly_token[0,20],
-          encrypted_password:[*1..9, *'A'..'Z', *'a'..'z'].sample(10).join,
-          # agreement: true
-      )
-    end
-
-    if @user.persisted?
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    callback_for(:facebook)
   end
 
   def twitter
-    # You need to implement the method below in your model
-    auth = request.env["omniauth.auth"]
-    @user = User.where(provider: auth.provider, provider_uid: auth.uid).first
-    unless @user
-      # ユーザが無い場合は作成
-      @user = User.create(
-          name:     auth.info.nickname,
-          email:    dummy_email(auth),
-          provider: auth.provider,
-          provider_token:    auth.credentials.token,
-          provider_uid: auth.uid,
-          password: Devise.friendly_token[0,20],
-          encrypted_password:[*1..9, *'A'..'Z', *'a'..'z'].sample(10).join,
-          # agreement: true
-      )
-    end
-
-    if @user.persisted?
-      set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
-    end
+    callback_for(:twitter)
   end
 
   def line
-    auth = request.env["omniauth.auth"]
-    @user = User.where(provider: auth.provider, provider_uid: auth.uid).first
-    unless @user
-      # ユーザが無い場合は作成
-      @user = User.create(
-          family_name:     auth.info.name,
-          given_name:     auth.info.name,
-          email:    dummy_email(auth),
-          provider: auth.provider,
-          provider_token:    auth.credentials.token,
-          provider_uid: auth.uid,
-          password: Devise.friendly_token[0,20],
-          encrypted_password:[*1..9, *'A'..'Z', *'a'..'z'].sample(10).join
-          # agreement: true
-      )
-    end
-
-    if @user.persisted?
-      set_flash_message(:notice, :success, kind: "Line") if is_navigational_format?
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.line_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to new_user_registration_url
-    end
+    callback_for(:line)
   end
 
   private
 
-  def dummy_email(auth)
-    "#{auth.provider}-#{auth.uid}@example.com"
+  def callback_for(provider)
+    auth = request.env["omniauth.auth"]
+    @user = User.where(provider: auth.provider, provider_uid: auth.uid).first
+    unless @user
+      @user = User.create(family_name:        auth.info.name,
+                          given_name:         auth.info.name,
+                          email:              "#{auth.provider}-#{auth.uid}@example.com",
+                          provider:           auth.provider,
+                          provider_token:     auth.credentials.token,
+                          provider_uid:       auth.uid,
+                          password:           Devise.friendly_token[0,20],
+                          encrypted_password: [*1..9, *'A'..'Z', *'a'..'z'].sample(10).join
+                          )
+    end
+    if @user.persisted?
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
   end
 end
